9b4a165a4df23e40eb7891dbcb88e7e5a4450a9f
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-06 17:41:28 -0500

    Adds functionality to add and remove items from the list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+  
+  
+// });
+
+
+const form = document.getElementById('create-task-form');
+const input = form.children[1];
+const submit = form.children[2];
+const list = document.getElementById('tasks');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newItem = document.createElement('li');
+  const newX = document.createElement('button');
+  newX.textContent = 'X';
+  newItem.textContent = `${input.value}  `;
+  list.append(newItem);
+  newItem.append(newX);
+  newX.addEventListener('click', () => newItem.remove());
+
+})
