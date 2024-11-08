375772cfb3740f7b9b4cb48de709bcf23a298702
Joseph Andrew Phillips <josephandrewphillips@gmail.com>
2024-11-08 12:50:11 -0500

    adds js functionality for color-coded task priority

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const form = document.getElementById('create-task-form');
 const input = form.children[1];
 const submit = form.children[2];
 const list = document.getElementById('tasks');
+const colors = document.getElementById('select');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -15,8 +16,8 @@ form.addEventListener('submit', (e) => {
   const newX = document.createElement('button');
   newX.textContent = 'X';
   newItem.textContent = `${input.value}  `;
+  newItem.style.color = colors.value;
   list.append(newItem);
   newItem.append(newX);
   newX.addEventListener('click', () => newItem.remove());
-
 })
